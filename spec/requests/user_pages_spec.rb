872a6321b57d0a47ab441d91b263cdf1e6c69bdc
Henrique <coura.henrique@gmail.com>
2014-03-24 22:41:36 -0300

    8-exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,17 +33,13 @@ describe "User pages" do
         it { should have_content('Email is invalid') }
         it { should have_content("Password can't be blank") }
         it { should have_content('Password is too short 
-                                 (minimum is 6 characters)') }
+                                (minimum is 6 characters)') }
       end
+
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      before { valid_user }
 
       describe "after saving the user" do
         before { click_button submit }
@@ -51,7 +47,7 @@ describe "User pages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
       end
 
       it "should create a user" do
---
+++ spec/support/user_pages_utilities.rb
@@ -0,0 +1,14 @@
+include ApplicationHelper
+
+def valid_user
+	fill_in "Name",         with: "Example User"
+    fill_in "Email",        with: "user@example.com"
+    fill_in "Password",     with: "foobar"
+    fill_in "Confirmation", with: "foobar"
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
